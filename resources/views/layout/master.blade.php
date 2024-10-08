25158ad30269b931633d5517c277508538edbd1f
projectaplikasi <rubyyjeniii@gmail.com>
2024-10-08 19:58:25 +0700

    secoundcommit

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,16 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
-    <link rel="stylesheet" href="{{secure_asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
     @include('sweetalert::alert')
     @yield('content')
-    <script src="{{secure_asset('js/datatables.js') }}"></script>
-    <script src="{{secure_asset('js/sweetalert.js') }}"></script>
-    <script src="{{secure_asset('js/jquery.js') }}"></script>
-    <script src="{{secure_asset('js/flowbite.js') }}"></script>
+    <script src="{{ asset('js/datatables.js') }}"></script>
+    <script src="{{ asset('js/sweetalert.js') }}"></script>
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/flowbite.js') }}"></script>
     @stack('script')
 </body>
 
